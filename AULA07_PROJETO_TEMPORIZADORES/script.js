9df5f3b524f08041282295995e9d6caf839b713a
lauxenmaju <161649077+lauxenmaju@users.noreply.github.com>
2024-09-23 10:34:20 -0300

    "first commit"

---
+++ AULA07_PROJETO_TEMPORIZADORES/script.js
@@ -0,0 +1,28 @@
+//setInterval 
+/*function acao(){
+    document.write('Executando ...</br>');
+}
+
+setInterval(acao,2000);*/
+
+//função anonima
+
+/*setInterval(()=>{
+    document.write('Executando2 ...</br>');
+},3000)
+
+//para parar a execução */
+
+/*var timer = setInterval(()=>{
+    document.write('Executando3 ...</br>');
+},1500);*/
+
+//outro temporizador - setTimeout
+
+function acao2(){
+    document.write('Executando2 ... </br>');
+}
+setTimeout(acao2,5000);
+
+
+
